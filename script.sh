62e442ac34de7fcc61f28c24e7c77f11f14f7c21
Vozmen <105993180+Vozmen@users.noreply.github.com>
2022-06-24 10:04:39 +0300

    Fin
    Fin

--- script.sh
+++ script.sh
@@ -20,19 +20,19 @@ wget https://raw.githubusercontent.com/Vozmen/OTUS9_BASH/main/access-4560-644067
 #Create script
 cat << EOF > /media/s.sh
 #!/bin/bash
-echo "#Обрабатываемый временной промежуток" > /media/log.log
+echo "#Processed time period" > /media/log.log
 cat /media/a.log | cut -d " " -f 4 | sort | sed -n '1 p; $ p;' | sed -e 's/^.//' >> /media/log.log
 echo " " >> /media/log.log
-echo "#Самое запрашиваемое доменное имя" >> /media/log.log
-cat /media/a.log | grep GET | grep -oE https?:\/\/[a-z]*[.][a-z]* | sort -rn |uniq -c |sort -rn | sed 's/^ *//' | sed -e '1! d' >> /media/log.log
+echo "#Most requested domain names" >> /media/log.log
+cat /media/a.log | grep GET | grep -oE https?:\/\/[a-z]*[.][a-z]* | sort -rn |uniq -c |sort -rn | sed 's/^ *//' | sed -e '1,10! d' >> /media/log.log
 echo " " >> /media/log.log
-echo "#Самый запрашиваемый IP" >> /media/log.log
-cat /media/a.log | grep GET | sort | cut -d " " -f 1 | uniq -c | sort -rn | sed 's/^ *//' | sed -e '1! d' >> /media/log.log
+echo "#Most requested IP adresses" >> /media/log.log
+cat /media/a.log | grep GET | sort | cut -d " " -f 1 | uniq -c | sort -rn | sed 's/^ *//' | sed -e '1,10! d' >> /media/log.log
 echo " " >> /media/log.log
-echo "#Все коды возврата" >> /media/log.log >> /media/log.log
+echo "#All exit codes" >> /media/log.log >> /media/log.log
 cat /media/a.log | grep -oE 'HTTP/1.1\" [0-9][0-9][0-9]' | cut -d " " -f 2 | sort -rn | uniq -c | sort -rn | sed 's/^ *//' >> /media/log.log
 echo " " >> /media/log.log
-echo "#Коды возврата ошибок" >> /media/log.log
+echo "#All error exit codes" >> /media/log.log
 cat /media/a.log | grep -oE 'HTTP/1.1\" [0-9][0-9][0-9]' | cut -d " " -f 2 | grep -E "(4[0-9][0-9]|5[0-9][0-9])" | sort -rn | uniq -c | sort -rn | sed 's/^ *//' >> /media/log.log
 EOF
 
